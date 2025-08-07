880c51738ff648202d7b37a52b8aef29e981a178
GiulianoFP <Giuliano15150@gmail.com>
2025-08-07 17:36:35 -0300

    -

---
+++ ej1/ej1/Models/Rectangulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej1.Models
+{
+public class Rectangulo
+    {
+        public double Ancho { get; set; }
+        public double Largo { get; set; }
+        public Rectangulo(double ancho,double largo)
+            {
+                Ancho = ancho;
+                Largo = largo;  
+            }
+        public double CalcularArea()
+            {
+                return Ancho * Largo;
+            }
+        public string Describir()
+            {
+                return $"Rectangulo:{Ancho},{Largo}";
+            }
+    }
+}
